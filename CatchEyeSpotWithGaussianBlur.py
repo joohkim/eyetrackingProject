a61e8e7f10a9d21a37e603e1678e768577de3785
kjh3551 <joohkim@gmarket.com>
2022-11-28 15:10:14 +0900

    update README.md

--- CatchEyeSpotWithGaussianBlur.py
+++ CatchEyeSpotWithGaussianBlur.py
@@ -12,7 +12,7 @@ while True:
     gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
 
-    _, threshold = cv2.threshold(gray_roi, 100, 255, cv2.THRESH_BINARY_INV)
+    _, threshold = cv2.threshold(gray_roi, 50, 255, cv2.THRESH_BINARY_INV)
 
     cv2.imshow("Threshold", threshold)
     cv2.imshow("gray roi", gray_roi)
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # eyetrackingProject
+눈동자의 움직임을 감지하여 좌표를 구해내는 프로젝트 
+
+##  Platform Information
+* python 3.8.5
+* openCV 4.6.0
+
+
