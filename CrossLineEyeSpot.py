2a49e2c81c3aae26df7e63e361336bb7480d7260
kjh3551 <joohkim@gmarket.com>
2022-12-03 03:43:31 +0900

    add Test.py

--- CrossLineEyeSpot.py
+++ CrossLineEyeSpot.py
@@ -12,7 +12,7 @@ while True:
     gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
 
-    _, threshold = cv2.threshold(gray_roi, 100, 255, cv2.THRESH_BINARY_INV)
+    _, threshold = cv2.threshold(gray_roi, 80, 255, cv2.THRESH_BINARY_INV)
     contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     cnt = contours[0]
@@ -23,7 +23,7 @@ while True:
         (x, y, w, h) = cv2.boundingRect(cnt)
         cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 2)
         cv2.line(roi, (x + int(w / 2), 0), (x + int(w / 2), rows), (0, 255, 0), 2)
-        cv2.line(roi, (0, y + int(h / 2), (cols, y + int(h / 2)), (0, 255, 0), 2))
+        # cv2.line(roi, (0, y + int(h / 2), (cols, y + int(h / 2)), (0, 255, 0), 2))
         # cv2.drawContours(roi, [cnt], -1, (0,0,255),3)
         break
 
--- README.md
+++ README.md
@@ -3,10 +3,16 @@
 
 ##  Platform Information
 * python 3.8.5
-* openCV 4.6.0
+* openCV 4.6.0.66
 
 ## install openCV
 ```shell
 pip install opencv-contrib-python
 ```
-
+## install dlib
+```shell
+pip install cmake
+```
+```shell
+pip install dlib==19.18.0
+```
