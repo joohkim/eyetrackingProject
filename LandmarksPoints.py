67ba8626c8ac5264babaa42479fe23401f3ec31a
kjh3551 <joohkim@gmarket.com>
2022-12-03 12:21:08 +0900

    Add eye_detection

--- LandmarksPoints.py
+++ LandmarksPoints.py
@@ -3,7 +3,9 @@ import numpy as np
 import dlib
 
 cap = cv2.VideoCapture(0)
+
 detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 while True:
     _, frame = cap.read()
@@ -13,7 +15,12 @@ while True:
     for face in faces:
         x, y = face.left(), face.top()
         x1, y1 = face.right(), face.bottom()
-        cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
+        #cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
+
+        landmarks = predictor(gray, face)
+        x = landmarks.part(36).x
+        y = landmarks.part(36).y
+        cv2.circle(frame, (x, y), 3, (0, 0, 255), 2)
         #print(face)
 
     cv2.imshow("Frame", frame)
